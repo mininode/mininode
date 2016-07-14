b3cc4da958b67c4ab9f6bce074d66bf3ecfb3cce
Alex Caudill <hypoalex@users.noreply.github.com>
2016-07-14 09:38:06 -0500

    Add empty per-module README

--- src/modules/os/os.c
+++ src/modules/os/os.c
@@ -4,7 +4,7 @@
 
 /*
  *  Duktape/C functions providing Node.js 'os' module functionality.
- *  See oss://nodejs.org/dist/v6.2.2/docs/api/os.html
+ *  See https://nodejs.org/dist/v6.2.2/docs/api/os.html
  */
 
 /*
@@ -18,7 +18,8 @@ const duk_number_list_entry mn_bi_os_consts[] = {
 	{ NULL, 0.0 }
 };
 
-duk_ret_t mn_bi_os(duk_context *ctx) {
+duk_ret_t
+mn_bi_os(duk_context *ctx) {
 	duk_push_object(ctx);
 	duk_put_function_list(ctx, -1, mn_bi_os_funcs);
 	duk_put_number_list(ctx, -1, mn_bi_os_consts);
