b4e0abaaa60b1bb8cdd4440df6564b695c998853
Alex Caudill <hypoalex@users.noreply.github.com>
2016-07-21 07:14:43 -0500

    Better error handling

--- src/modules/fs/methods/access.c
+++ src/modules/fs/methods/access.c
@@ -1,8 +1,8 @@
 #include "mininode.h"
 #include <unistd.h>
-#include <fcntl.h>
 
-duk_ret_t mn_bi_fs_access(duk_context *ctx) {
+duk_ret_t
+mn_bi_fs_access(duk_context *ctx) {
 	const int nargs = duk_get_top(ctx);
 	const char *path = NULL;
 	uv_fs_t *req = NULL;
@@ -11,10 +11,10 @@ duk_ret_t mn_bi_fs_access(duk_context *ctx) {
 	if (nargs == 3) {
 		mode = duk_require_int(ctx, -2);
 		path = duk_require_string(ctx, -3);
-	} else if (nargs == 2){
-		mode = F_OK;
+	} else if (nargs == 2) {
+		mode = F_OK | R_OK;
 		path = duk_require_string(ctx, -2);
-	} else if (nargs < 2) {
+	} else if (nargs < 2 || nargs > 3) {
 		duk_pop_n(ctx, nargs);
 		duk_push_error_object(
 			ctx,
@@ -25,7 +25,17 @@ duk_ret_t mn_bi_fs_access(duk_context *ctx) {
 		duk_put_prop_string(ctx, -2, "fileName");
 		duk_push_int(ctx, 17);
 		duk_put_prop_string(ctx, -2, "lineNumber");
+		/*
+		 * All Error objects should set fileName & lineNumber.
+		 * This is a bit of a maintenance nightmare, but I feel
+		 * it's worth the effort to maintain this convention.
+		 */
 		duk_throw(ctx);
+		/*
+		 * Note that we can't really return after we throw.
+		 * Return values are used to indicate the depth of
+		 * the return value stack. It's a meta thing, okay?
+		 */
 		return 1;
 	}
 
