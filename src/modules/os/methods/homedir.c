eecfc94ad8fca12ecd19a4ef6047fd20749e407b
Alex Caudill <hypoalex@users.noreply.github.com>
2016-07-31 17:13:25 -0500

    Use uv_os_homedir()

--- src/modules/os/methods/homedir.c
+++ src/modules/os/methods/homedir.c
@@ -1,10 +1,18 @@
 #include "duktape.h"
 #include "mininode.h"
+#include "uv.h"
 #include <unistd.h>
-#include <pwd.h>
 
 duk_ret_t mn_bi_os_homedir(duk_context *ctx) {
-	struct passwd *pwent = getpwuid(getuid());
-	duk_push_string(ctx, pwent->pw_dir);
+        char buf[PATH_MAX];
+        size_t len = sizeof(buf);
+        const int err = uv_os_homedir(buf, &len);
+	
+	if (err) {
+		duk_push_string(ctx, "uv_os_homedir() error!");
+		duk_throw(ctx);
+	}
+
+	duk_push_string(ctx, buf);
 	return 1;
 }
