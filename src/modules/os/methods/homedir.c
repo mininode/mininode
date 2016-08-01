2c6e5a89de690b365ff89d6a4ea994c13450ade2
Alex Caudill <hypoalex@users.noreply.github.com>
2016-08-01 10:58:16 -0500

    Whitespace changes

--- src/modules/os/methods/homedir.c
+++ src/modules/os/methods/homedir.c
@@ -4,10 +4,10 @@
 #include <unistd.h>
 
 duk_ret_t mn_bi_os_homedir(duk_context *ctx) {
-        char buf[PATH_MAX];
-        size_t len = sizeof(buf);
-        const int err = uv_os_homedir(buf, &len);
-	
+	char buf[PATH_MAX];
+	size_t len = sizeof(buf);
+	const int err = uv_os_homedir(buf, &len);
+
 	if (err) {
 		duk_push_string(ctx, "uv_os_homedir() error!");
 		duk_throw(ctx);
