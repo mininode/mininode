3646f8e97cf1b1186c9fe1551c2dbdc526e4a3cc
Alex Caudill <alex@memphisinsights.com>
2017-03-21 07:50:37 -0500

    Use uv_os_tmpdir() rather than static string

--- src/modules/os/methods/tmpdir.c
+++ src/modules/os/methods/tmpdir.c
@@ -1,8 +1,14 @@
 #include "duktape.h"
 #include "mininode.h"
+#include "uv.h"
 
 duk_ret_t
 mn_bi_os_tmpdir(duk_context *ctx) {
-	duk_push_string(ctx, "/tmp");
+	char  *buf = NULL;
+	size_t len = NULL;
+
+	uv_os_tmpdir(buf, len);
+
+	duk_push_string(ctx, buf);
 	return 1;
 }
