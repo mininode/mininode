0c3e235cdf38de19eafab56eea445c735f1b22e7
Alex Caudill <alex@memphisinsights.com>
2017-03-21 08:30:06 -0500

    Take the address of len, exception branch on buf

--- src/modules/os/methods/tmpdir.c
+++ src/modules/os/methods/tmpdir.c
@@ -5,10 +5,15 @@
 duk_ret_t
 mn_bi_os_tmpdir(duk_context *ctx) {
 	char  *buf = NULL;
-	size_t len = NULL;
+	size_t len = 0;
 
-	uv_os_tmpdir(buf, len);
+	uv_os_tmpdir(buf, &len);
 
-	duk_push_string(ctx, buf);
-	return 1;
+	if (buf) {
+		duk_push_string(ctx, buf);
+		return 1;
+	} else {
+		duk_push_string(ctx, "uv_os_tmpdir() failed!");
+		duk_throw(ctx);
+	}
 }
